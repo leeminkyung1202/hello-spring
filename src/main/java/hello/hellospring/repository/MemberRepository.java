838f69dc49193f639f9eeab3cfa07411342bedd0
Ellie <lmk941202@cocen.com>
2023-11-08 11:36:53 +0900

    Feat:회원 도메인과 리포지토리 생성

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional; // 데이터가 없으면 NULL로 반환되는데, Optional로 감싸서 반환되는 방법을 선호
+
+public interface MemberRepository {
+    Member save(Member member); // 회원을 저장하고, 저장된 회원 반환
+    Optional<Member> findById(Long id); // 저장된 회원 id로 찾기
+    Optional<Member> findByName(String name); // 저장된 회원 name으로 찾기
+    List<Member> findAll(); // 저장 된 모든 회원 리스트 반환
+}
---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L; // sequence는 0,1,2.. 키값을 생성해줌.
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); // 저장 시 id 값을 올려줌.
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream() // 회원 리스트를 꺼내서 루프 돌림
+                .filter(member -> member.getName().equals(name)) // 검색한 name과 같은 값인지 확인
+                .findAny(); // 찾으면 바로 반환 (없으면 Optional로 감싸서 NULL 반환)
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
