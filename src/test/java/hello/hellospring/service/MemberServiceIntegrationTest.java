d88f572e0243dfa5d1b2bc9668a570f5217c089e
Ellie <lmk941202@cocen.com>
2023-11-27 11:52:44 +0900

    Feat:스프링 데이터 JPA 사용해보기

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -24,7 +24,7 @@ class MemberServiceIntegrationTest {
     public void 회원가입() {
         //given(주어짐)
         Member member = new Member();
-        member.setName("spring5");
+        member.setName("spring001");
 
         //when(실행)
         Long saveId = memberService.join(member);
@@ -38,10 +38,10 @@ class MemberServiceIntegrationTest {
     public void 중복_회원_예외() {
         //given
         Member member1 = new Member();
-        member1.setName("spring");
+        member1.setName("spring3");
 
         Member member2 = new Member();
-        member2.setName("spring");
+        member2.setName("spring3");
 
         //when
         memberService.join(member1);
